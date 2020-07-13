30872dd6f20d22aecdeadd413049c5b824f9e3e9
Ismael Faro <Ismael.Faro1@ibm.com>
2020-07-12 23:51:56 -0400

    add x gate

--- quantum_simulator.py
+++ quantum_simulator.py
@@ -4,16 +4,29 @@ from math import cos,sin,pi
 r2=0.70710678118 # 1/sqrt(2)
 
 class QuantumCircuit:
-    pass
+    def __init__(self, qubits):
+        if (qubits == 0):
+            print('Number of Qubits need to ne more than 0')
+        self.Qubits = qubits
+        self.Bits = qubits
+        self.circuit = []
+    
+    def add_gate(self, gate):
+        self.circuit.append(gate)
+    
+    def x(self, qubit):
+        self.add_gate(['x',qubit])
 
+    def __repr__(self):
+        return str(self.circuit)
 class QuantumSimulator:
     pass
 
 if __name__ == "__main__":
     print("Quantum Simulator for Developers project")
-    # qc = QuantumCircuit(5)
-    # qc.x(0)
-    # qc.rx(0,2)
+    qc = QuantumCircuit(5)
+    qc.x(0)
+    print(qc)
     # qc.x(1)
     # qc.x(0)
     # qc.x(2)
