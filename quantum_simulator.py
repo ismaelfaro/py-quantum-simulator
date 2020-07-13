95816751bba978f5c3cec6890b36b20246b54944
Ismael Faro <Ismael.Faro1@ibm.com>
2020-07-12 23:40:35 -0400

    basic componenets

---
+++ quantum_simulator.py
@@ -0,0 +1,38 @@
+import random
+from math import cos,sin,pi
+
+r2=0.70710678118 # 1/sqrt(2)
+
+class QuantumCircuit:
+    pass
+
+class QuantumSimulator:
+    pass
+
+if __name__ == "__main__":
+    print("Quantum Simulator for Developers project")
+    # qc = QuantumCircuit(5)
+    # qc.x(0)
+    # qc.rx(0,2)
+    # qc.x(1)
+    # qc.x(0)
+    # qc.x(2)
+    # qc.z(0)
+    # qc.x(0)
+    
+    # qc.h(2)
+    # qc.h(0)
+    # qc.h(1)
+    
+    # qc.cx(0,1)
+    # qc.cx(0,1)
+    # qc.m(0,0)
+    
+    # print(qc.circuit)
+    
+    # quantumSimulator =  QuantumSimulator(qc)
+    # stateVector = quantumSimulator.run("statevector")
+    # print(stateVector)
+    # result = quantumSimulator.run("counts", 1024)
+    # print(result)
+    pass
