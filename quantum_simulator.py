fde51f170a026f10f02450807b2c9bed5f808327
Ismael Faro <Ismael.Faro1@ibm.com>
2020-07-13 00:01:19 -0400

    quantum Circuit class completed

--- quantum_simulator.py
+++ quantum_simulator.py
@@ -17,6 +17,37 @@ class QuantumCircuit:
     def x(self, qubit):
         self.add_gate(['x',qubit])
 
+    def z(self, qubit):
+        self.rz(qubit,pi) 
+
+    def y(self, qubit):
+        self.rz(qubit,pi)
+        self.x(qubit)
+
+    def h(self, qubit):
+        self.add_gate(['h',qubit])
+
+    def cx(self, qubit, target):
+        self.add_gate(['cx',qubit,target])
+
+    def rx(self, qubit, theta):
+        self.add_gate(['rx',qubit, theta])
+    
+    def ry(self, qubit, theta):
+        self.rx(qubit,pi/2)
+        self.h(qubit)
+        self.rx(qubit,theta)
+        self.h(qubit)
+        self.rx(qubit,-pi/2)
+
+    def rz(self, qubit, theta):
+        self.h(qubit)
+        self.rx(qubit,theta)
+        self.h(qubit)
+
+    def m(self, qubit, target):
+        self.add_gate(['m',qubit,target])
+
     def __repr__(self):
         return str(self.circuit)
 class QuantumSimulator:
@@ -26,21 +57,22 @@ if __name__ == "__main__":
     print("Quantum Simulator for Developers project")
     qc = QuantumCircuit(5)
     qc.x(0)
+    
+    qc.x(1)
+    qc.x(0)
+    qc.x(2)
+    qc.z(0)
+    qc.x(0)
+    
+    qc.h(2)
+    qc.h(0)
+    qc.h(1)
+    
+    qc.cx(0,1)
+    qc.cx(0,1)
+    qc.m(0,0)
+
     print(qc)
-    # qc.x(1)
-    # qc.x(0)
-    # qc.x(2)
-    # qc.z(0)
-    # qc.x(0)
-    
-    # qc.h(2)
-    # qc.h(0)
-    # qc.h(1)
-    
-    # qc.cx(0,1)
-    # qc.cx(0,1)
-    # qc.m(0,0)
-    
     # print(qc.circuit)
     
     # quantumSimulator =  QuantumSimulator(qc)
