bc21cc65c66c7d0b756884c554e679c392295f91
Ismael Faro <Ismael.Faro1@ibm.com>
2020-07-15 02:46:28 -0400

    update

--- quantum_simulator.py
+++ quantum_simulator.py
@@ -11,21 +11,4 @@ class QuantumSimulator:
 
 if __name__ == "__main__":
     print("Quantum Simulator for Developers project")
-    # qc = QuantumCircuit(2)
-    # qc.x(0)
-    # qc.h(0)
-    # qc.x(1)
-    # qc.rx(0,pi/2)
-    # qc.z(0)
-    # qc.h(1)
-    # qc.cx(0,1)
-    # qc.cx(1,0)
-    # qc.m(0,0)
-    
-    # print(qc)
-    
-    # quantumSimulator =  QuantumSimulator(qc)
-
-    # result = quantumSimulator.run(1024, "counts")
-    
-    # print(result)
+  
